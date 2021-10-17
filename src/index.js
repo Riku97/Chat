6227de79fbf6237c3e0c024b2c94525e045f64bd
Riku97 <vic.sax.29@gmail.com>
2021-10-17 17:56:50 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const app = require("express")();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+//Rutas
+app.get('/', function(req, res) {
+    res.sendFile( __dirname + '/public/Emitir.html');
+  });
+
+app.get('/ver', function(req, res) {
+    res.sendFile( __dirname + '/public/Ver.html');
+});
+
+//Socket io para escuchar conexiones
+io.on('connection', (socket)=>{
+
+    console.log("Nueva conexion");
+    
+    //Aqui definimos que debe de hacer la nueva conexion detectada.
+
+    //si recibimos un evento "nuevo mensaje"
+    socket.on("nuevo mensaje", (msj)=>{
+        io.emit('nuevo mensaje', msj);
+    });
+
+    //si recibimos un evento "streaming"
+    socket.on("streaming",(image)=>{
+        socket.broadcast.emit('stream', image);
+    });
+});
+io.on('connect', ()=>{
+    console.log("Usuario conectado.")
+});
+//si se desconecta un usuario.
+io.on('disconnect', ()=>{
+    console.log("Usuario desconectado");
+});
+
+http.listen(port, () => {
+    console.log("Servidor en puerto 3000."); 
+});
